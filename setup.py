d945cd748454e0c4894e903898271df5895ba527
Alex Davies <traverse.da@gmail.com>
2016-09-17 10:25:14 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='pynit',
+      version='0.1',
+      description='Service management in python',
+      author='Alex Davies',
+      author_email='traverse.da@gmail.com',
+      url='https://github.com/traverseda/pynit',
+      packages=['pynit',],
+     )
